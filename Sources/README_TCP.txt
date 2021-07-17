34ee69d49c19953f2e8b9b6b9a1e42be2553d2d9
Abrar Alam <abraralam246@gmail.com>
2021-07-16 21:56:54 -0400

    Add additional info. to the README file

--- Sources/README_TCP.txt
+++
@@ -1,3 +0,0 @@
-I successfully created a client and a server, however, some timing bugs still persist. For example, 
-my server fails to decode my sent stream (I see blank strings). This will be fixed in 2 days. I have all the
-design ready, which just needs to be implemented
--- Sources/main.py
+++ Sources/main.py
@@ -1,10 +1,5 @@
-from edge import Edge
-from cell import Cell
 from grid import Grid
 from maze_generator import MazeGenerator
-from random import seed
-from random import randint
-from random import choice
 import pygame
 
 screen = pygame.display.set_mode((640, 480))
@@ -16,7 +11,6 @@ myGrid = Grid((8, 8))
 myGrid.create_grid((8, 8))
 myMaze = MazeGenerator(myGrid)
 myMaze.processGrid()  # Our maze is now fully initialized
-# resume from here
 
 num_of_rows = len(myGrid.list_of_rows)
 num_of_cols = len(myGrid.list_of_rows[0])
@@ -26,9 +20,7 @@ while running:
     if event.type == pygame.QUIT:
         running = 0
     screen.fill(white)
-    #pygame.draw.line(screen, black , (25, 0), (25, 25))
-    #pygame.draw.line(screen, black , (25, 25), (50, 25))
-    # pygame.draw.aaline(screen, (0, 0, 255), (639, 0), (0, 479))
+
     for i in range(0, num_of_rows):
         for j in range(0, num_of_cols):
             if(myMaze.mazedGrid.list_of_rows[i][j].top_edge is not None):
@@ -47,8 +39,4 @@ while running:
                 pygame.draw.line(
                     screen, black, myGrid.list_of_rows[i][j].left_edge.point_1, myGrid.list_of_rows[i][j].left_edge.point_2)
 
-            #pygame.draw.line(screen, black , myGrid.list_of_rows[i][j].top_edge.point_1, myGrid.list_of_rows[i][j].top_edge.point_2)
-            #pygame.draw.line(screen, black , myGrid.list_of_rows[i][j].bottom_edge.point_1, myGrid.list_of_rows[i][j].bottom_edge.point_2)
-            #pygame.draw.line(screen, black , myGrid.list_of_rows[i][j].right_edge.point_1, myGrid.list_of_rows[i][j].right_edge.point_2)
-            #pygame.draw.line(screen, black , myGrid.list_of_rows[i][j].left_edge.point_1, myGrid.list_of_rows[i][j].left_edge.point_2)
     pygame.display.flip()
